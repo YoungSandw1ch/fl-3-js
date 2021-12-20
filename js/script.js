70a0a37c92ac512e8bec10cccf52a13bfca5b502
YoungSandw1ch <sersus1991@gmail.com>
2021-12-20 22:45:44 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,25 @@ let userName = 'Вася';
 user = userName;
 console.log(user);
 
+
+let userHeight = 145 / 0;
+console.log(userHeight);
+
+let userNameV1;
+console.log(typeof userNameV1);
+
+let userSize = "45" / "8";
+console.log(userSize);
+
+
+console.log('35' + - "22");
+
+console.log('35' * "22");
+
+console.log('588' >userSize++);
+
+let usersCounter = 0;
+let newUsers = usersCounter++;
+console.log(newUsers);
+
+console.log(!false && 11 || 18 && !'');
