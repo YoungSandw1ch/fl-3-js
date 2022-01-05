efd339a7f208000b31de2daa3685de1c00591ba0
Sergei Mikhailenko <sersus1991@gmail.com>
2022-01-05 22:26:34 +0200

    some tast for cart

--- js/object.js
+++ js/object.js
@@ -101,59 +101,56 @@
  * { name: '🍓', price: 110 }
  */
 
-// const cart = {
-//   items: [],
+const cart = {
+  items: [],
+  getItems() {
+    return this.items;
+  },
+  add(product) {
+    //     product.quantity = 1;
+    //     this.items.push(product);
+    //     for (const item of cart.items) {
+    //     }
+    //     product.quantity = 1;
+    // console.log(product.name);
+    // if (product.name === ) {
+    //   product.quantity += 1;
+    // }
+    // else {
+    this.items.push(product);
+    // }
+  },
+  remove(productName) {
+    for (let i = 0; i < this.items.length; i += 1) {
+      const item = this.items[i];
+      console.log(item);
 
-//   getItems() {
-//     return this.items;
-//   },
+      if (productName === item.name) {
+        this.items.splice(i, 1);
+        break;
+      }
+    }
+  },
+  clear() {
+    this.items = [];
+  },
+  countTotalPrice() {},
+  increaseQuantity(productName) {},
+  decreaseQuantity(productName) {},
+};
 
-//   add(product) {
-//     product.quantity = 1;
-//     this.items.push(product);
+console.table(cart.getItems());
 
-//     for (const item of cart.items) {
-//     }
-//     product.quantity = 1;
-
-// console.log(product.name);
-// if (product.name === ) {
-//   product.quantity += 1;
-// }
-// else {
-// this.items.push(product);
-// }
-// },
-
-// remove(productName) {
-// for (const item of cart.items) {
-//   console.log(item);
-// }
-// const allProductsName = object.values(item);
-// this.items.splice(index, 1);
-// },
-
-//   clear() {
-//     this.items = [];
-//   },
-
-//   countTotalPrice() {},
-//   increaseQuantity(productName) {},
-//   decreaseQuantity(productName) {},
-// };
-
-// console.table(cart.getItems());
-
-// cart.add({ name: '🍎', price: 50 });
-// cart.add({ name: '🍋', price: 60 });
-// cart.add({ name: '🍋', price: 60 });
-// cart.add({ name: '🍓', price: 110 });
+cart.add({ name: '🍎', price: 50 });
+cart.add({ name: '🍋', price: 60 });
+cart.add({ name: '🍋', price: 60 });
+cart.add({ name: '🍓', price: 110 });
 
 // console.table(cart.getItems());
 // console.log(cart.getItems());
 
-// cart.remove('🍎');
-// console.table(cart.getItems());
+cart.remove('🍎');
+console.table(cart.getItems());
 
 // cart.clear();
 // console.table(cart.getItems());
@@ -168,18 +165,18 @@
 // console.log('Total: ', cart.countTotalPrice());
 //=========================================================
 
-const books = [
-  {
-    title: 'The Last Kingdom',
-    author: 'Bernard Cornwell',
-    rating: 8.38,
-  },
-  {
-    title: 'На берегу спокойных вод',
-    author: 'Роберт Шекли',
-    rating: 8.51,
-  },
-];
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'На берегу спокойных вод',
+//     author: 'Роберт Шекли',
+//     rating: 8.51,
+//   },
+// ];
 
 // for (const book of books) {
 //   console.log(book.title);
@@ -187,13 +184,13 @@ const books = [
 //   console.log(book.rating);
 // }
 
-for (const book of books) {
-  const { title, author, rating } = book;
+// for (const book of books) {
+//   const { title, author, rating } = book;
 
-  console.log(title);
-  console.log(author);
-  console.log(rating);
-}
+//   console.log(title);
+//   console.log(author);
+//   console.log(rating);
+// }
 //=========================================================
 //=========================================================
 //=========================================================
