57bd79d1eeb98db87ca69a6268c018ec03f338b5
Sergei Mikhailenko <sersus1991@gmail.com>
2022-01-05 15:33:41 +0200

    Update object.js

--- js/object.js
+++ js/object.js
@@ -103,9 +103,20 @@
 
 const cart = {
   items: [],
-  getItems() {},
-  add(product) {},
-  remove(productName) {},
+  getItems() {
+    return this.items;
+  },
+  add(product) {
+    product.quantity = 1;
+    this.items.push(product);
+  },
+  remove(productName) {
+    // for (const item of cart.items) {
+    //   console.log(item);
+    // }
+    // const allProductsName = object.values(item);
+    // this.items.splice(index, 1);
+  },
   clear() {},
   countTotalPrice() {},
   increaseQuantity(productName) {},
@@ -119,7 +130,8 @@ cart.add({ name: '🍋', price: 60 });
 cart.add({ name: '🍋', price: 60 });
 cart.add({ name: '🍓', price: 110 });
 
-// console.table(cart.getItems());
+console.table(cart.getItems());
+// console.log(cart.getItems());
 
 cart.remove('🍎');
 // console.table(cart.getItems());
