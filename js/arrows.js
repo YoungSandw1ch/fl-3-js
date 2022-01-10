0c9622afecf022eed335ac9ffe4ffa16c9630787
YoungSandw1ch <sersus1991@gmail.com>
2022-01-10 20:21:12 +0200

    arrows

---
+++ js/arrows.js
@@ -0,0 +1,9 @@
+// const add = function (a, b, c) {
+//   console.log(a, b, c);
+//   return a + b + c;
+// };
+
+// console.log(add(5, 10, 15));
+
+const add = (a, b, c) => a + b + c;
+console.log(add(5, 10, 15));
