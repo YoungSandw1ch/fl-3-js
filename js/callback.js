26cf31035bc1d1ed3146b22dc0d36482b7d9505e
Sergei Mikhailenko <sersus1991@gmail.com>
2022-01-20 19:02:35 +0200

    Update callback.js

--- js/callback.js
+++ js/callback.js
@@ -78,48 +78,49 @@
 
 //====================================================
 
-const tweets = [
-  { id: '000', likes: 5, tags: ['js', 'nodejs'] },
-  { id: '001', likes: 2, tags: ['html', 'css'] },
-  { id: '002', likes: 17, tags: ['html', 'js', 'nodejs'] },
-  { id: '003', likes: 8, tags: ['css', 'react'] },
-  { id: '004', likes: 0, tags: ['js', 'nodejs', 'react'] },
-];
+// const tweets = [
+//   { id: '000', likes: 5, tags: ['js', 'nodejs'] },
+//   { id: '001', likes: 2, tags: ['html', 'css'] },
+//   { id: '002', likes: 17, tags: ['html', 'js', 'nodejs'] },
+//   { id: '003', likes: 8, tags: ['css', 'react'] },
+//   { id: '004', likes: 0, tags: ['js', 'nodejs', 'react'] },
+// ];
 
-const getTags = tweets =>
-  tweets.reduce((allTags, tweet) => {
-    allTags.push(...tweet.tags);
+// const getTags = tweets =>
+//   tweets.reduce((allTags, tweet) => {
+//     allTags.push(...tweet.tags);
 
-    // console.log(allTags);
-    return allTags;
-  }, []);
+//     // console.log(allTags);
+//     return allTags;
+//   }, []);
 
-const tags = getTags(tweets);
-// console.log(tags);
-console.log(`getTags(tweets): `, getTags(tweets));
-// Вынесем callback-функцию отдельно, а в reducе передадим ссылку на нее.
-// Это стандартная практика если callback-функция довольно большая.
+// const tags = getTags(tweets);
+// // console.log(tags);
+// console.log(`getTags(tweets): `, getTags(tweets));
+// // Вынесем callback-функцию отдельно, а в reducе передадим ссылку на нее.
+// // Это стандартная практика если callback-функция довольно большая.
 
-// Если в объекте-аккумуляторе acc нету своего свойства с ключем tag,
-// то создаем его и записывает ему значение 0.
-// В противном случае увеличиваем значение на 1.
-const getTagStats = (acc, tag) => {
-  if (!acc.hasOwnProperty(tag)) {
-    acc[tag] = 0;
-  }
+// // Если в объекте-аккумуляторе acc нету своего свойства с ключем tag,
+// // то создаем его и записывает ему значение 0.
+// // В противном случае увеличиваем значение на 1.
+// const getTagStats = (acc, tag) => {
+//   if (!acc.hasOwnProperty(tag)) {
+//     acc[tag] = 0;
+//   }
 
-  acc[tag] += 1;
+//   acc[tag] += 1;
 
-  return acc;
-};
+//   return acc;
+// };
 
-console.log(getTagStats);
-// Начальное значение аккумулятора это пустой объект {}
-const countTags = tags => tags.reduce(getTagStats, {});
+// console.log(getTagStats);
+// // Начальное значение аккумулятора это пустой объект {}
+// const countTags = tags => tags.reduce(getTagStats, {});
 
-const tagCount = countTags(tags);
-console.log(tagCount);
+// const tagCount = countTags(tags);
+// console.log(tagCount);
 //====================================================
+
 //====================================================
 //====================================================
 //====================================================
