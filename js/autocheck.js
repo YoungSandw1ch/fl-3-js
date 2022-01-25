dcfe8898dcb347d3223566e17cebd113a965f7fa
Sergei Mikhailenko <sersus1991@gmail.com>
2022-01-25 18:29:20 +0200

    Update autocheck.js

--- js/autocheck.js
+++ js/autocheck.js
@@ -565,6 +565,116 @@
 // console.log(authorsInReversedOrder);
 //===================================================
 
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     rating: 8.51,
+//   },
+//   {
+//     title: 'The Dream of a Ridiculous Man',
+//     author: 'Fyodor Dostoevsky',
+//     rating: 7.75,
+//   },
+//   { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+//   { title: 'Enemy of God', author: 'Bernard Cornwell', rating: 8.67 },
+// ];
+
+// const sortedByAuthorName = [...books].sort((prev, next) =>
+//   prev.author.localeCompare(next.author),
+// );
+// console.table(sortedByAuthorName);
+
+// const sortedByReversedAuthorName = [...sortedByAuthorName].reverse();
+// console.table(sortedByReversedAuthorName);
+
+// const sortedByAscendingRating = [...books].sort((a, b) => a.rating - b.rating);
+// console.table(sortedByAscendingRating);
+
+// const sortedByDescentingRating = [...books].sort(
+//   (prev, next) => next.rating - prev.rating,
+// );
+// console.table(sortedByDescentingRating);
+
+//===================================================
+// const users = [
+//   {
+//     name: 'Moore Hensley',
+//     email: 'moorehensley@indexia.com',
+//     eyeColor: 'blue',
+//     friends: ['Sharron Pace'],
+//     isActive: false,
+//     balance: 2811,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Sharlene Bush',
+//     email: 'sharlenebush@tubesys.com',
+//     eyeColor: 'blue',
+//     friends: ['Briana Decker', 'Sharron Pace'],
+//     isActive: true,
+//     balance: 3821,
+//     gender: 'female',
+//   },
+//   {
+//     name: 'Ross Vazquez',
+//     email: 'rossvazquez@xinware.com',
+//     eyeColor: 'green',
+//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//     isActive: false,
+//     balance: 3793,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Elma Head',
+//     email: 'elmahead@omatom.com',
+//     eyeColor: 'green',
+//     friends: ['Goldie Gentry', 'Aisha Tran'],
+//     isActive: true,
+//     balance: 2278,
+//     gender: 'female',
+//   },
+//   {
+//     name: 'Carey Barr',
+//     email: 'careybarr@nurali.com',
+//     eyeColor: 'blue',
+//     friends: ['Jordan Sampson', 'Eddie Strong'],
+//     isActive: true,
+//     balance: 3951,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Blackburn Dotson',
+//     email: 'blackburndotson@furnigeer.com',
+//     eyeColor: 'brown',
+//     friends: ['Jacklyn Lucas', 'Linda Chapman'],
+//     isActive: false,
+//     balance: 1498,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Sheree Anthony',
+//     email: 'shereeanthony@kog.com',
+//     eyeColor: 'brown',
+//     friends: ['Goldie Gentry', 'Briana Decker'],
+//     isActive: true,
+//     balance: 2764,
+//     gender: 'female',
+//   },
+// ];
+
+// const sortByName = users =>
+//   [...users].sort((a, b) => a.name.localeCompare(b.name));
+
+// console.table(sortByName(users));
+
+//===================================================
+
 const books = [
   {
     title: 'The Last Kingdom',
@@ -582,27 +692,21 @@ const books = [
     rating: 7.75,
   },
   { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
-  { title: 'Enemy of God', author: 'Bernard Cornwell', rating: 8.67 },
+  {
+    title: 'The Dreams in the Witch House',
+    author: 'Howard Lovecraft',
+    rating: 8.67,
+  },
 ];
+const MIN_BOOK_RATING = 8;
+// Change code below this line
 
-const sortedByAuthorName = [...books].sort((prev, next) =>
-  prev.author.localeCompare(next.author),
-);
-console.table(sortedByAuthorName);
+const names = [...books]
+  .filter(book => book.rating >= MIN_BOOK_RATING)
+  .sort((a, b) => a.author.localeCompare(b.author))
+  .map(book => book.author);
 
-const sortedByReversedAuthorName = [...sortedByAuthorName].reverse();
-console.table(sortedByReversedAuthorName);
-
-const sortedByAscendingRating = [...books].sort((a, b) => a.rating - b.rating);
-console.table(sortedByAscendingRating);
-
-const sortedByDescentingRating = [...books].sort(
-  (prev, next) => next.rating - prev.rating,
-);
-console.table(sortedByDescentingRating);
-
-//===================================================
-//===================================================
+console.log(names);
 //===================================================
 //===================================================
 //===================================================
