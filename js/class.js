d130daac1060bc7bfa2e60fa18814166fe4b2755
Sergei Mikhailenko <sersus1991@gmail.com>
2022-07-13 17:25:57 +0300

    examples 3 of 3 9 lesson

--- js/class.js
+++ js/class.js
@@ -308,14 +308,97 @@
 // console.log(secondToggle.on);
 // console.groupEnd('secondToggle');
 /*
- *=================================================================
+ *================Модуль 5. Занятие 9 example 1====================
  */
+// const chopShop = {
+//   stones: [
+//     { name: 'Emerald', price: 1300, quantity: 4 },
+//     { name: 'Diamond', price: 2700, quantity: 3 },
+//     { name: 'Sapphire', price: 1400, quantity: 7 },
+//     { name: 'Ruby', price: 800, quantity: 2 },
+//   ],
+
+//   // calcTotalPrice(stoneName) {
+//   //   return this.stones.reduce(
+//   //     (acc, e) => acc + (e.name === stoneName ? e.price * e.quantity : 0),
+//   //     0,
+//   //   );
+//   // },
+
+//   calcTotalPrice(stoneName) {
+//     const { price, quantity } = this.stones.find(
+//       ({ name }) => name === stoneName,
+//     );
+//     return price * quantity;
+//   },
+// };
+
+// console.log(chopShop.calcTotalPrice('Emerald')); // 5200
+// console.log(chopShop.calcTotalPrice('Diamond')); // 8100
+// console.log(chopShop.calcTotalPrice('Sapphire')); // 9800
+// console.log(chopShop.calcTotalPrice('Ruby')); // 1600
+
 /*
- *=================================================================
+ *=============Example 2 - Телефонная книга========================
  */
+// const phonebook = {
+//   contacts: [],
+
+//   add(contact) {
+//     const newContact = {
+//       list: 'default',
+//       ...contact,
+//       id: this.generateId(),
+//       createdAt: this.getDate(),
+//     };
+
+//     this.contacts.push(newContact);
+//   },
+
+//   generateId() {
+//     return '_' + Math.random().toString(36).substr(2, 9);
+//   },
+
+//   getDate() {
+//     return Date.now();
+//   },
+// };
+
+// phonebook.add({
+//   name: 'Mango',
+//   email: 'mango@mail.com',
+//   list: 'friends',
+// });
+// console.log(phonebook);
+
+// phonebook.add({
+//   name: 'Poly',
+//   email: 'poly@hotmail.com',
+// });
+// console.log(phonebook);
 /*
- *=================================================================
+ *===================Example 3 - Калькулятор=======================
  */
+// const calculator = {
+//   read(a, b) {
+//     this.a = a;
+//     this.b = b;
+//   },
+
+//   add() {
+//     return this.a + this.b;
+//   },
+
+//   mult() {
+//     return this.a * this.b;
+//   },
+// };
+
+// console.log(calculator);
+// calculator.read(10, 20);
+// console.log(calculator);
+// console.log(calculator.add());
+// console.log(calculator.mult());
 /*
  *=================================================================
  */
