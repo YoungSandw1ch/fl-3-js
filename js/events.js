dccbb7883a8e85fa53c258c02c4f2949d4e325b0
Sergei Mikhailenko <sersus1991@gmail.com>
2022-08-15 11:29:17 +0300

    fix filter button

--- js/events.js
+++ js/events.js
@@ -1,25 +1,51 @@
 const refs = {
   box: document.querySelector('.box'),
 };
-let tags = new Set();
 
 refs.box.addEventListener('click', onBtnClick);
+
+/*
+ *----выбор любого количества тегов и сбор их в колекцию
+ */
+// let tags = new Set();
+
+// function onBtnClick(e) {
+//   if (e.target.nodeName !== 'BUTTON') return;
+
+//   const btn = e.target;
+//   const value = btn.dataset.value;
+//   const isBtnActive = btn.classList.contains('btn--active');
+
+//   if (isBtnActive) {
+//     //есть тег в колекции? уже есть значит убираем его
+//     tags.delete(value);
+//   } else {
+//     // нету тега в коллекции ? значит добавим его
+//     tags.add(value);
+//   }
+
+//   e.target.classList.toggle('btn--active');
+//   console.log(tags);
+// }
+
+/*
+ *----выбор только одного тега c возможностью отжать тег
+ */
 
 function onBtnClick(e) {
   if (e.target.nodeName !== 'BUTTON') return;
 
-  const btn = e.target;
-  const value = btn.dataset.value;
-  const isBtnActive = btn.classList.contains('btn--active');
+  const activeBtn = document.querySelector('.btn--active');
 
-  if (isBtnActive) {
-    //есть тег в колекции? уже есть значит убираем его
-    tags.delete(value);
-  } else {
-    // нету тега в коллекции ? значит добавим его
-    tags.add(value);
+  if (activeBtn) {
+    activeBtn.classList.remove('btn--active');
   }
 
-  e.target.classList.toggle('btn--active');
-  console.log(tags);
+  if (activeBtn?.dataset?.value === e.target.dataset.value) {
+    activeBtn.classList.remove('btn--active');
+    console.log('Пожалуйста выберите тег');
+  } else {
+    e.target.classList.add('btn--active');
+    console.log(e.target.dataset.value);
+  }
 }
