2b332abed71910ebf7dc6e15a5e18b0a836ec1e9
Sergei Mikhailenko <sersus1991@gmail.com>
2022-08-16 15:58:13 +0300

    repeat material

--- js/events.js
+++ js/events.js
@@ -1,8 +1,8 @@
-const refs = {
-  box: document.querySelector('.box'),
-};
+// const refs = {
+//   box: document.querySelector('.box'),
+// };
 
-refs.box.addEventListener('click', onBtnClick);
+// refs.box.addEventListener('click', onBtnClick);
 
 /*
  *----выбор любого количества тегов и сбор их в колекцию
@@ -32,20 +32,49 @@ refs.box.addEventListener('click', onBtnClick);
  *----выбор только одного тега c возможностью отжать тег
  */
 
-function onBtnClick(e) {
-  if (e.target.nodeName !== 'BUTTON') return;
+// function onBtnClick(e) {
+//   if (e.target.nodeName !== 'BUTTON') return;
 
-  const activeBtn = document.querySelector('.btn--active');
+//   const activeBtn = document.querySelector('.btn--active');
 
-  if (activeBtn) {
-    activeBtn.classList.remove('btn--active');
-  }
+//   if (activeBtn) {
+//     activeBtn.classList.remove('btn--active');
+//   }
 
-  if (activeBtn?.dataset?.value === e.target.dataset.value) {
-    activeBtn.classList.remove('btn--active');
-    console.log('Пожалуйста выберите тег');
-  } else {
-    e.target.classList.add('btn--active');
-    console.log(e.target.dataset.value);
-  }
-}
+//   if (activeBtn?.dataset?.value === e.target.dataset.value) {
+//     activeBtn.classList.remove('btn--active');
+//     console.log('Пожалуйста выберите тег');
+//   } else {
+//     e.target.classList.add('btn--active');
+//     console.log(e.target.dataset.value);
+//   }
+// }
+
+/*
+ *----------------------просто повторил материал
+ */
+// const box = document.querySelector('.box');
+// const selectedTags = new Set();
+
+// box.addEventListener('click', onBtnClick);
+
+// function onBtnClick(e) {
+//   if (e.target.nodeName !== 'BUTTON') return;
+
+//   const activeBtn = e.target;
+//   activeBtn.classList.toggle('btn--active');
+
+//   const isBtnActive = activeBtn.classList.contains('btn--active');
+//   const tag = activeBtn.dataset.value;
+
+//   if (!isBtnActive) {
+//     selectedTags.delete(tag);
+//   } else {
+//     selectedTags.add(tag);
+//   }
+//   // just try work with sets
+//   // const filteredTags = Array.from(selectedTags).filter(e => e !== '#js');
+//   // console.log('filtered: ', filteredTags);
+
+//   console.log(selectedTags);
+// }
