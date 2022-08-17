30a657690588b0b61ffbb68f4844ac35a3300331
Sergei Mikhailenko <sersus1991@gmail.com>
2022-08-17 16:56:26 +0300

    finish gallery simpleligthbox

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -4,16 +4,23 @@ const galleryRef = document.querySelector('.gallery');
 const galleryItemsMarkup = createItemsMarkup(galleryItems);
 
 galleryRef.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+galleryRef.addEventListener('click', onGalleryImgClick);
+var galleyLightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt' });
 
 function createItemsMarkup(items) {
   return items
     .map(
       ({ preview, original, description }) => `
-      <div class="gallery__item">
         <a class="gallery__item" href="${original}">
           <img class="gallery__image" src="${preview}" alt="${description}" />
-        </a>
-      </div> `,
+        </a>`,
     )
     .join('');
+}
+
+function onGalleryImgClick(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains('gallery__image')) return;
+
+  galleyLightbox.open(e.target);
 }
