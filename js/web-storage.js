89aaad236f6dbdb5756a402567ae1c880f04612d
Sergei Mikhailenko <sersus1991@gmail.com>
2022-08-31 11:28:21 +0300

    web storage form-feedback work

--- js/web-storage.js
+++ js/web-storage.js
@@ -3,22 +3,44 @@ import throttle from 'lodash.throttle';
 const refs = {
   form: document.querySelector('.form'),
   name: document.querySelector('input[name="name"]'),
-  sex: document.querySelector('.radio'),
   message: document.querySelector('textarea[name="message"]'),
 };
 
-// console.log(refs.sex);
-// console.log(refs.form);
-// console.log(refs.name);
-// console.log(refs.message);
+const STORAGE_KEY = 'savedDataForm';
+//----обьект для сохранения данных в локальное хранилище-----------------
+const savedData = { ...JSON.parse(localStorage.getItem(STORAGE_KEY)) }; // если что то сохранено то распыляем его в наш обьект иначе при перезагрузке мы перезапишем наш обьект на пустой
+//---возвращаем данные с локального хранилища-----------------------------
+returnDataFromLocalStorage();
+//------------------------------------------------------------------------
 
-refs.form.addEventListener('click', onSubmitPush);
-refs.name.addEventListener('input', throttle(onInputName, 500));
+refs.form.addEventListener('submit', onSubmitPush);
+refs.form.addEventListener('input', throttle(onFormInputsChange, 500));
+refs.form.addEventListener('change', onFormInputsChange);
 
 function onSubmitPush(e) {
   e.preventDefault();
+
+  console.log('send form');
+
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
 }
 
-function onInputName(e) {
-  console.log(e.target.value);
+function onFormInputsChange(e) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(savedData));
+  savedData[e.target.name] = e.target.value;
+}
+
+function returnDataFromLocalStorage() {
+  const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (!data) return; //проверка чтоб не было ошибки в деструктуризации нижн "not found"
+
+  const { name, sex, message } = data;
+
+  if (name) refs.name.value = name;
+  if (message) refs.message.value = message;
+  if (sex) {
+    const checkedRadioBtn = document.querySelector(`input[value="${sex}"]`);
+    checkedRadioBtn.setAttribute('checked', true);
+  }
 }
