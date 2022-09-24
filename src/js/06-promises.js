da1e5fdfcdcf999e8b1660fec16eb5dfce6b2f04
Sergei Mikhailenko <sersus1991@gmail.com>
2022-09-24 20:50:20 +0300

    add style for hippodrome

--- src/js/06-promises.js
+++ src/js/06-promises.js
@@ -1,1 +1,8 @@
 import '../css/promises.css';
+import BSN from 'bootstrap.native';
+
+const modal = new BSN.Modal('#exampleModal');
+
+const refs = {
+  modal: document.querySelector('#exampleModal'),
+};
