b52a02143602bcd60efd0c617d79bfd882c306c0
Sergei Mikhailenko <sersus1991@gmail.com>
2023-04-22 22:33:55 +0300

    need repeat ajax and http requests

--- js/queryString.js
+++ js/queryString.js
@@ -1,9 +1,46 @@
-import { useSearchParams } from 'react-router-dom';
+// import { useSearchParams } from 'react-router-dom';
 
-const [searchParams] = useSearchParams();
+// const [searchParams] = useSearchParams();
 
-const params = useMemo(
-  () => Object.fromEntries([...searchParams]),
-  [searchParams]
-);
-const { name, maxPrice, inStock } = params;
+// const params = useMemo(
+//   () => Object.fromEntries([...searchParams]),
+//   [searchParams]
+// );
+// const { name, maxPrice, inStock } = params;
+
+import { useNavigate } from 'react-router-dom';
+
+const URL = 'https://pokeapi.co/api/v2';
+
+const LoginForm = () => {
+  return (
+    <form>
+      <input type="text" name="login" value="Login">
+      <button type="submit">enter</button>
+    </form>
+  );
+};
+
+const FetchPokemon = async pokemon => {
+  const res = await fetch(`${URL}/pokemon/${pokemon}`);
+  console.log(res);
+  return JSON.parse(res);
+};
+
+export const Login = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = async name => {
+    const response = await FakeAPI(name);
+    if (response.success) {
+      navigate('/profile', { replace: true });
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <LoginForm onSubmit={handleSubmit} />
+    </div>
+  );
+};
